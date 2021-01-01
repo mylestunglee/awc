15b8cae022ce96dc8188a1539ab09dae45af88ed
Myles Lee <mylestunglee@gmail.com>
2021-01-01 17:17:40 +0000

    add remaining auxiliary variables

--- optimise.c
+++ optimise.c
@@ -4,36 +4,69 @@
 #include <stdio.h>
 #define symbolic_name_length 16
 
-static void add_distribution_auxiliary_vector(glp_prob* const problem, int* const auxiliary_offset) {
+static void add_distribution_rows(glp_prob* const problem, int* const row_offset) {
 	glp_add_rows(problem, model_capacity);
 
 	for (model_t model = 0; model < model_capacity; ++model) {
 		char name[symbolic_name_length];
 		snprintf(name, symbolic_name_length, "u_"model_format, model + 1);
-		glp_set_row_name(problem, *auxiliary_offset, name);
-		glp_set_row_bnds(problem, *auxiliary_offset, GLP_DB, 0.0, 1.0);
-		++*auxiliary_offset;
+		glp_set_row_name(problem, *row_offset, name);
+		glp_set_row_bnds(problem, *row_offset, GLP_DB, 0.0, 1.0);
+		++*row_offset;
 	}
 }
 
-static void add_allocation_auxiliary_vector(
+static void add_allocation_rows(
 	glp_prob* const problem,
 	const tile_wide_t buildable_allocations[model_capacity],
-	int* const auxiliary_offset) {
+	int* const row_offset) {
 
 	glp_add_rows(problem, model_capacity);
 
 	for (model_t model = 0; model < model_capacity; ++model) {
 		char name[symbolic_name_length];
 		snprintf(name, symbolic_name_length, "v_"model_format, model + 1);
-		glp_set_row_name(problem, *auxiliary_offset, name);
+		glp_set_row_name(problem, *row_offset, name);
 
+		// Row lower and upper bounds cannot be equal with GLP_DB
 		if (buildable_allocations[model])
-			glp_set_row_bnds(problem, *auxiliary_offset, GLP_DB, 0.0, (double)buildable_allocations[model]);
+			glp_set_row_bnds(problem, *row_offset, GLP_DB, 0.0, (double)buildable_allocations[model]);
 		else
-			glp_set_row_bnds(problem, *auxiliary_offset, GLP_FX, 0.0, 0.0);
-		++*auxiliary_offset;
+			glp_set_row_bnds(problem, *row_offset, GLP_FX, 0.0, 0.0);
+
+		++*row_offset;
 	}
+}
+
+static void add_cost_row(glp_prob* const problem, const gold_t budget, int* const row_offset) {
+	glp_add_rows(problem, 1);
+	glp_set_row_name(problem, *row_offset, "b");
+	glp_set_row_bnds(problem, *row_offset, GLP_UP, 0.0, budget);
+	++*row_offset;
+}
+
+static void add_surplus_rows(glp_prob* const problem, int* const row_offset) {
+	glp_add_rows(problem, model_capacity);
+
+	for (model_t model = 0; model < model_capacity; ++model) {
+		char name[symbolic_name_length];
+		snprintf(name, symbolic_name_length, "s_"model_format, model + 1);
+		glp_set_row_name(problem, *row_offset, name);
+		glp_set_row_bnds(problem, *row_offset, GLP_LO, 0.0, 0.0);
+		++*row_offset;
+	}
+}
+
+static void add_rows(
+	glp_prob* const problem,
+	const tile_wide_t buildable_allocations[model_capacity],
+	const gold_t budget) {
+
+	int row_offset = 1;
+	add_distribution_rows(problem, &row_offset);
+	add_allocation_rows(problem, buildable_allocations, &row_offset);
+	add_cost_row(problem, budget, &row_offset);
+	add_surplus_rows(problem, &row_offset);
 }
 
 void optimise_build_allocations(
@@ -45,17 +78,12 @@ void optimise_build_allocations(
 
 	(void)friendly_distribution;
 	(void)enemy_distribution;
-	(void)buildable_allocations,
-	(void)budget;
 	(void)build_allocation;
 
 	glp_prob* const problem = glp_create_prob();
 	glp_set_prob_name(problem, "build_allocations");
 	glp_set_obj_dir(problem, GLP_MAX);
-
-	int auxiliary_offset = 1;
-	add_distribution_auxiliary_vector(problem, &auxiliary_offset);
-	add_allocation_auxiliary_vector(problem, buildable_allocations, &auxiliary_offset);
+	add_rows(problem, buildable_allocations, budget);
 
 	glp_simplex(problem, NULL);
 	(void)problem;
