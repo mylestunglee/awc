863f90aed1fc4a69859a0bf5dbef37f293f5e3e2
Myles Lee <mylestunglee@gmail.com>
2022-02-20 18:05:32 +0000

    write construction of rows and columns

--- optimise2.c
+++ optimise2.c
@@ -1,10 +1,10 @@
 #include "optimise2.h"
 #include "unit_constants.h"
+#include <assert.h>
 #include <stdio.h>
 
-#define FOR(variable, condition)                                               \
-    for (index_t variable = 0; variable < MODEL_CAPACITY; ++variable)          \
-        if (condition)
+#define FOR(variable)                                                          \
+    for (index_t variable = 0; variable < MODEL_CAPACITY; ++variable)
 
 bool a_cell_exists(const struct bap_inputs* const inputs, const index_t i,
                    const index_t j) {
@@ -13,32 +13,32 @@ bool a_cell_exists(const struct bap_inputs* const inputs, const index_t i,
 }
 
 bool a_row_exists(const struct bap_inputs* const inputs, const index_t i) {
-    FOR(j, !a_cell_exists(inputs, i, j)) return false;
+    FOR(j) if (!a_cell_exists(inputs, i, j)) return false;
 
     return true;
 }
 
 bool a_column_exists(const struct bap_inputs* const inputs, const index_t j) {
-    FOR(i, !a_cell_exists(inputs, i, j)) return false;
+    FOR(i) if (!a_cell_exists(inputs, i, j)) return false;
 
     return true;
 }
 
 bool b_cell_exists(const struct bap_inputs* const inputs, const index_t i,
                    const index_t j) {
-    FOR(i, !a_cell_exists(inputs, i, j)) return false;
+    FOR(i) if (!a_cell_exists(inputs, i, j)) return false;
 
     return true;
 }
 
 bool b_row_exists(const struct bap_inputs* const inputs, const index_t i) {
-    FOR(j, !b_cell_exists(inputs, i, j)) return false;
+    FOR(j) if (!b_cell_exists(inputs, i, j)) return false;
 
     return true;
 }
 
 bool b_column_exists(const struct bap_inputs* const inputs, const index_t j) {
-    FOR(i, !b_cell_exists(inputs, i, j)) return false;
+    FOR(i) if (!b_cell_exists(inputs, i, j)) return false;
 
     return true;
 }
@@ -47,28 +47,142 @@ bool s_column_exists(const struct bap_inputs* const inputs, const index_t j) {
     return a_column_exists(inputs, j) || b_column_exists(inputs, j);
 }
 
+bool problem_exists(const struct bap_inputs* const inputs) {
+    FOR(j) if (!s_column_exists(inputs, j)) return false;
+
+    return true;
+}
+
 #define SYMBOLIC_NAME_LENGTH 16
 
 void create_row(struct bap_temps* const temps, const char variable_name,
-                const index_t variable_index, int bounds_type,
-                double lower_bound, double upper_bound) {
+                const index_t variable_index, const int bounds_type,
+                const double lower_bound, const double upper_bound) {
+    assert('a' <= variable_name && variable_name < 'z');
+    assert(lower_bound <= upper_bound);
+
     char name[SYMBOLIC_NAME_LENGTH];
-    snprintf(name, SYMBOLIC_NAME_LENGTH, "%c_%hu", variable_name,
-             variable_index);
+    snprintf(name, sizeof name, "%c_%hu", variable_name, variable_index);
     glp_set_row_name(temps->problem, temps->curr_index, name);
     glp_set_row_bnds(temps->problem, temps->curr_index, bounds_type,
                      lower_bound, upper_bound);
     ++temps->curr_index;
 }
 
-void create_a_rows(const struct bap_inputs* const inputs,
-                   struct bap_temps* const temps) {
-    temps->a_row_index = temps->curr_index;
-    FOR(i, a_row_exists(inputs, i))
-    create_row(temps, 'a', i, GLP_DB, 0.0, inputs->friendly_distribution[i]);
+void create_distribution_rows(const struct bap_inputs* const inputs,
+                              struct bap_temps* const temps) {
+    temps->distribution_row_index = temps->curr_index;
+    FOR(i)
+    if (a_row_exists(inputs, i))
+        create_row(temps, 'd', i, GLP_FX, inputs->friendly_distribution[i],
+                   inputs->friendly_distribution[i]);
+}
+
+void create_allocation_rows(const struct bap_inputs* const inputs,
+                            struct bap_temps* const temps) {
+    temps->allocation_row_index = temps->curr_index;
+    for (tile_t capturable = 0; capturable < CAPTURABLE_CAPACITY; ++capturable)
+        if (inputs->capturables[capturable] > 0)
+            create_row(temps, 'a', capturable, GLP_UP, 0.0,
+                       (double)inputs->capturables[capturable]);
+}
+
+void create_budget_row(const struct bap_inputs* const inputs,
+                       struct bap_temps* const temps) {
+    temps->budget_row_index = temps->curr_index;
+    create_row(temps, 'b', 0, GLP_UP, 0.0, inputs->budget);
+}
+
+void create_surplus_rows(const struct bap_inputs* const inputs,
+                         struct bap_temps* const temps) {
+    temps->surplus_row_index = temps->curr_index;
+    FOR(j)
+    if (s_column_exists(inputs, j))
+        create_row(temps, 's', j, GLP_LO, 0.0, 0.0);
+}
+
+void create_rows(const struct bap_inputs* const inputs,
+                 struct bap_temps* const temps) {
+    temps->curr_index = 0;
+    create_distribution_rows(inputs, temps);
+    create_allocation_rows(inputs, temps);
+    create_budget_row(inputs, temps);
+    create_surplus_rows(inputs, temps);
+}
+
+void create_matrix_column(struct bap_temps* const temps,
+                          const char variable_name, const index_t i,
+                          const index_t j, const int bounds_type,
+                          const double upper_bound, const int variable_type) {
+    assert('A' <= variable_name && variable_name <= 'Z');
+    assert(0.0 <= upper_bound);
+
+    char name[SYMBOLIC_NAME_LENGTH];
+    snprintf(name, sizeof name, "%c_%hu_%hu", variable_name, i, j);
+    glp_set_col_name(temps->problem, temps->curr_index, name);
+    glp_set_col_bnds(temps->problem, temps->curr_index, bounds_type, 0.0,
+                     upper_bound);
+    glp_set_col_kind(temps->problem, temps->curr_index, variable_type);
+    ++temps->curr_index;
 }
 
 void create_a_columns(const struct bap_inputs* const inputs,
                       struct bap_temps* const temps) {
     temps->a_column_index = temps->curr_index;
+    FOR(i)
+    FOR(j)
+    if (a_cell_exists(inputs, i, j))
+        create_matrix_column(temps, 'A', i, j, GLP_DB,
+                             (double)inputs->friendly_distribution[i], GLP_CV);
 }
+
+void create_b_columns(const struct bap_inputs* const inputs,
+                      struct bap_temps* const temps) {
+    temps->b_column_index = temps->curr_index;
+    FOR(i)
+    FOR(j)
+    if (b_cell_exists(inputs, i, j))
+        create_matrix_column(temps, 'B', i, j, GLP_LO, 0.0, GLP_IV);
+}
+
+void create_objective_column(struct bap_temps* const temps) {
+    temps->z_column_index = temps->curr_index;
+    glp_set_col_name(temps->problem, temps->curr_index, "z");
+    glp_set_obj_coef(temps->problem, temps->curr_index, 1.0);
+    glp_set_col_bnds(temps->problem, temps->curr_index, GLP_FR, 0.0, 0.0);
+    ++temps->curr_index;
+}
+
+void create_columns(const struct bap_inputs* const inputs,
+                    struct bap_temps* const temps) {
+    temps->curr_index = 0;
+    create_a_columns(inputs, temps);
+    create_b_columns(inputs, temps);
+    create_objective_column(temps);
+}
+
+void sparse_matrix_set(struct bap_temps* const temps, const int row,
+                       const int column, const double value) {
+    ++temps->curr_index;
+    temps->matrix_rows[temps->curr_index] = row + 1;
+    temps->matrix_columns[temps->curr_index] = column + 1;
+    temps->matrix_values[temps->curr_index] = value;
+}
+
+void set_distribution_submatrix(const struct bap_inputs* const inputs,
+                                struct bap_temps* const temps) {
+    FOR(i)
+    FOR(j)
+    if (a_cell_exists(inputs, i, j))
+        sparse_matrix_set(temps, temps->distribution_row_index + i, temps->a_column_index + j, 1.0);
+}
+
+/*
+void set_allocation_submatrix(const struct bap_inputs* const inputs,
+                                struct bap_temps* const temps) {
+    for(tile_t capturable = 0; capturable < CAPTURABLE_CAPACITY; ++capturable)
+    for (model_t i = buildable_models[capturable]; i < buildable_models[capturable + 1]; ++i)
+    FOR(j)
+    sparse_matrix_set(temps, temps->allocation_row_index + i, temps->b_column_index + j, 1.0);   
+}
+*/
--- optimise2.h
+++ optimise2.h
@@ -14,6 +14,7 @@ struct bap_inputs {
 
 typedef int16_t index_t;
 
+#ifdef expose_optimise_internals
 bool a_cell_exists(const struct bap_inputs* const, const index_t,
                    const index_t);
 bool a_row_exists(const struct bap_inputs* const, const index_t);
@@ -23,6 +24,22 @@ bool b_cell_exists(const struct bap_inputs* const, const index_t,
 bool b_row_exists(const struct bap_inputs* const, const index_t);
 bool b_column_exists(const struct bap_inputs* const, const index_t);
 bool s_column_exists(const struct bap_inputs* const, const index_t);
+bool problem_exists(const struct bap_inputs* const);
+
+void create_row(struct bap_temps* const, const char variable, const index_t,
+                const int, const double, const double upper_bound);
+void create_distribution_rows(const struct bap_inputs* const,
+                              struct bap_temps* const);
+void create_allocation_rows(const struct bap_inputs* const,
+                            struct bap_temps* const);
+void create_budget_row(const struct bap_inputs* const, struct bap_temps* const);
+void create_surplus_rows(const struct bap_inputs* const,
+                         struct bap_temps* const);
+void create_rows(const struct bap_inputs* const, struct bap_temps* const);
+
+#endif
+
+#define SPARSE_MATRIX_LENGTH (MODEL_CAPACITY * MODEL_CAPACITY * MODEL_CAPACITY)
 
 struct bap_temps {
     glp_prob* const problem;
@@ -30,10 +47,13 @@ struct bap_temps {
     index_t a_column_index;
     index_t b_column_index;
     index_t z_column_index;
-    index_t a_row_index;
-    index_t b_row_index;
-    index_t s_row_index;
-    index_t f_row_index;
+    index_t distribution_row_index;
+    index_t allocation_row_index;
+    index_t budget_row_index;
+    index_t surplus_row_index;
+    int matrix_rows[SPARSE_MATRIX_LENGTH];
+    int matrix_columns[SPARSE_MATRIX_LENGTH];
+    double matrix_values[SPARSE_MATRIX_LENGTH];
 };
 
 #endif
