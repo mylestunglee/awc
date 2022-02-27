5b66913173b27fea785ba6f52018430e28cf28e1
Myles Lee <mylestunglee@gmail.com>
2022-02-27 22:20:12 +0000

    fix glpk solver bugs

--- test/bap_glpk_solver_test.cpp
+++ test/bap_glpk_solver_test.cpp
@@ -1,6 +1,44 @@
 #define EXPOSE_BAP_GLPK_SOLVER_INTERNALS
+#include "../constants.h"
+#include "../unit_constants.h"
 #include "bap_glpk_solver_fixture.hpp"
+#include "test_constants.hpp"
+
+constexpr auto CAPTURABLE_FACTORY = 1;
+constexpr auto CAPTURABLE_AIRPORT = 2;
 
 TEST_F(bap_glpk_solver_fixture, bap_glpk_solve_null_problem) {
+    ASSERT_NE(solve(), 0);
+}
+
+TEST_F(bap_glpk_solver_fixture, bap_glpk_solve_single_infantry) {
+    inputs->enemy_distribution[MODEL_INFANTRY] = 1;
+    inputs->capturables[CAPTURABLE_FACTORY] = 1;
+    inputs->budget = models_cost[MODEL_INFANTRY];
     ASSERT_EQ(solve(), 0);
+    ASSERT_EQ(outputs[MODEL_INFANTRY], 1);
+}
+
+TEST_F(bap_glpk_solver_fixture, bap_glpk_solve_constrained_by_capturables) {
+    inputs->enemy_distribution[MODEL_INFANTRY] = 1;
+    inputs->capturables[CAPTURABLE_FACTORY] = 2;
+    inputs->budget = 3 * models_cost[MODEL_INFANTRY];
+    ASSERT_EQ(solve(), 0);
+    ASSERT_EQ(outputs[MODEL_INFANTRY], 2);
+}
+
+TEST_F(bap_glpk_solver_fixture, bap_glpk_solve_constrained_by_budget) {
+    inputs->enemy_distribution[MODEL_INFANTRY] = 1;
+    inputs->capturables[CAPTURABLE_FACTORY] = 3;
+    inputs->budget = 2 * models_cost[MODEL_INFANTRY];
+    ASSERT_EQ(solve(), 0);
+    ASSERT_EQ(outputs[MODEL_INFANTRY], 2);
+}
+
+TEST_F(bap_glpk_solver_fixture, bap_glpk_solve_selects_most_effective_unit) {
+    inputs->enemy_distribution[MODEL_HELICOPTER] = 1;
+    inputs->capturables[CAPTURABLE_AIRPORT] = 1;
+    inputs->budget = 100 * GOLD_SCALE;
+    ASSERT_EQ(solve(), 0);
+    ASSERT_EQ(outputs[MODEL_FIGHTER], 1);
 }
--- test/test_constants.hpp
+++ test/test_constants.hpp
@@ -2,10 +2,13 @@
 #define TEST_CONSTANTS_H
 
 constexpr tile_t TILE_PLAINS = 1;
+constexpr tile_t TILE_FACTORY = 11;
+
 constexpr model_t MODEL_INFANTRY = 0;
 constexpr model_t MODEL_ARTILLERY = 5;
 constexpr model_t MODEL_MISSLES = 8;
 constexpr model_t MODEL_HELICOPTER = 9;
+constexpr model_t MODEL_FIGHTER = 10;
 constexpr model_t MODEL_SUBMARINE = 12;
 
 #endif
